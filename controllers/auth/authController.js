499d43f4d67a39b6c411f9700aebe30b31c00725
Vanshika454 <110763792+Vanshika454@users.noreply.github.com>
2024-09-04 22:19:53 +0530

    Update authController.js

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -3,78 +3,79 @@ const validateRequest = require('./services/validate-request');
 const passwordManager = require('./services/password-manager');
 const { createToken } = require('./services/create-token'); 
 
-//Creating  User
 module.exports.registerUser = async (req, res) => {
     const { name, userName, email, password } = req.body;
 
+    // Validate request data
     try {
         validateRequest.validateRegister(name, userName, email, password);
     } catch (error) {
-        res.status(400).send(error.message);
-        return;
+        return res.status(400).send(error.message);
     }
-    
 
     try {
-        let existingUserName = await User.findOne({ userName });
-        if(existingUserName) {
-            res.status(400).send('Existing Username!');
-            return;
+        // Check for existing username
+        const existingUserName = await User.findOne({ userName });
+        if (existingUserName) {
+            return res.status(400).send('Username already exists');
         }
 
-        let existingUserEmail = await User.findOne({ email });
-        if(existingUserEmail) {
-            res.status(400).send('Existing Email!');
-            return;
+        // Check for existing email
+        const existingUserEmail = await User.findOne({ email });
+        if (existingUserEmail) {
+            return res.status(400).send('Email already exists');
         }
 
+        // Hash password and create new user
         const hashedPW = await passwordManager.toHash(password);
-        await User.create({
-            name, userName, email, password: hashedPW
-        });
+        const newUser = new User({ name, userName, email, password: hashedPW });
+        await newUser.save();
 
-        res.status(200).send({ message: 'User created succefully...!!!' });
+        res.status(201).send({ message: 'User created successfully' });
 
     } catch (error) {
-        res.status(500).send('Something went wrong!');
+        console.error('Registration error:', error);
+        res.status(500).send('Internal server error');
     }
-}
+};
 
-// Log In User
 module.exports.loginUser = async (req, res) => {
     const { userName, password } = req.body;
 
+    // Validate request data
     try {
         validateRequest.validateLogIn(userName, password);
     } catch (error) {
-        res.status(400).send(error.message);
-        return;
+        return res.status(400).send(error.message);
     }
 
     try {
+        // Find user by username
         const user = await User.findOne({ userName });
         if (!user) {
-            res.status(401).send('User does not exist!');
-            return;
+            return res.status(401).send('User does not exist');
         }
         
-        const isPasswordCorrect = passwordManager.compare(password, user.password);
+        // Compare passwords
+        const isPasswordCorrect = await passwordManager.compare(password, user.password);
         if (isPasswordCorrect) {
+            // Create token and respond
             const token = createToken(user._id, user.userName, user.email);
             const { password, ...userWithoutPassword } = user.toObject();
 
             res
                 .status(200)
-                .cookie('jwt', token)
-                .cookie('userId', `${user._id.toString()}`)
+                .cookie('jwt', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' })
+                .cookie('userId', user._id.toString(), { httpOnly: true, secure: process.env.NODE_ENV === 'production' })
                 .send({
                     message: 'Logged in successfully',
                     user: userWithoutPassword
                 });
         } else {
-            res.status(401).send('Password is incorrect');
+            res.status(401).send('Incorrect password');
         }
-    } catch(err) {
-        res.status(500).send('Something went wrong!');
+    } catch (error) {
+        console.error('Login error:', error);
+        res.status(500).send('Internal server error');
     }
 };
